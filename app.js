f54607caabd99738859546c2287993994be0fcb7
silfarias17 <silvifarias17@gmail.com>
2023-10-10 12:16:42 -0300

    proyecto

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.listen(3000, () => {
+    console.log('Listening on port http://localhost:3000');
+})
